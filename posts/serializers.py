cdba188d521adc74f4d700b6c6a89963ef84cd20
Raziyeh <raziyehmohajer.rms@gmail.com>
2023-11-25 22:28:06 +0330

    Add average score and number of scores in posts api

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,11 +5,17 @@ from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    average_score = serializers.FloatField(source="average_score.score", read_only=True)
+    scores_number = serializers.FloatField(
+        source="average_score.number", read_only=True
+    )
 
     class Meta:
         model = Post
         read_only_fields = (
             "id",
+            "average_score",
+            "scores_number",
             "created_at",
             "updated_at",
         )
@@ -20,4 +26,6 @@ class PostSerializer(serializers.ModelSerializer):
             "content",
             "created_at",
             "updated_at",
+            "average_score",
+            "scores_number",
         )
